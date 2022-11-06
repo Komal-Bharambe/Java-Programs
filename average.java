ff77a27c2bac3fb0d30aa1dec66002e008348226
Komal Bharambe <116254499+Komal-Bharambe@users.noreply.github.com>
2022-11-06 19:25:30 +0530

    Add files via upload

---
+++ average.java
@@ -0,0 +1,9 @@
+class average{
+    public static void main(String args[]) {
+        int sum = 0;
+        for(int i = 1; i<25; i++){
+            sum += i;
+            System.out.println(sum/25);
+        }
+    }
+}
