92de26757392c406f1860697b2fe55f5abff3f47
Komal Bharambe <116254499+Komal-Bharambe@users.noreply.github.com>
2022-11-06 19:24:40 +0530

    Add files via upload

---
+++ area_circle.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class area_circle{
+    public static void main(String args[]) {
+        double a;
+        a = 3.14;
+        int r = 4;
+        System.out.println(a*r*r);
+         
+    }
+}
