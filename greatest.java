1f2daba60f8d930be937f18083f904feced58ddf
Komal Bharambe <116254499+Komal-Bharambe@users.noreply.github.com>
2022-11-08 13:42:37 +0530

    Add files via upload

---
+++ greatest.java
@@ -0,0 +1,12 @@
+class greatest{
+    public static void main(String args[]) {
+        int a = 2, b = 5;
+        if(a>b){
+            System.out.println(a);
+        }
+        else{
+            System.out.println(b);
+        }
+        
+    }
+}
